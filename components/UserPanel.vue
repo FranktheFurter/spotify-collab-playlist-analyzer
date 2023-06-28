11ee842f70432a36c6bd91d5b33557a1570d8f87
Marc Frankfurter <marcfrankfurter@gmail.com>
2023-06-28 22:54:57 +0200

    big fat progress

--- components/UserPanel.vue
+++ components/UserPanel.vue
@@ -12,11 +12,17 @@ const { userData } = await useSpotifyUserApi(props.userId);
 </script>
 
 <template>
-  <div v-if="userData">
-    <!-- <h2>User Information</h2>
-    <p>User ID: {{ userData.id }}</p> -->
+  <div v-if="userData" class="flex flex-col items-center">
+    <img
+      v-if="userData.images && userData.images.length > 0"
+      class="rounded-full"
+      :src="userData.images[0].url"
+      alt="Playlist cover image"
+      width="30"
+      height="30"
+    />
+    <div v-else class="w-30px h-30px bg-gray rounded-full"></div>
     <h3>{{ userData.display_name }}</h3>
-    <!-- Add additional fields here -->
   </div>
 </template>
 
