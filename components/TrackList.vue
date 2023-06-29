8835ffe497c8da8e3cbfd2f358941cbea6245d9f
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-06-29 15:21:11 +0200

    progress

--- components/TrackList.vue
+++ components/TrackList.vue
@@ -1,22 +1,30 @@
 <script lang="ts" setup>
 const { playlist } = useSpotifyPlaylistApi()
 const { getDisplayNameById } = useSpotifyPlaylistDataHelper()
+
+const getArtistNames = (artists: any[]) => {
+  return artists.map((artist: { name: any }) => artist.name).join(", ")
+}
 </script>
 
 <template>
-  <div v-if="playlist" class="grid grid-cols-8 gap-8 px-8">
+  <div v-if="playlist" class="flex flex-wrap justify-between">
     <div
       v-for="(track, index) in playlist"
       :key="index"
-      class="aspect-1/1 bg-gray w-100%"
+      class="w-1/8 bg-gray-200/10 m-2 flex flex-col"
     >
       <nuxt-img
         v-if="track.track.album.images && track.track.album.images[0]"
         :src="track.track.album.images[0].url"
         alt="cover"
         loading="lazy"
-        class="object-contain w-100%"
+        class="object-contain w-full"
       />
+      <div class="flex-grow">
+        <p class="line-clamp-2">{{ track.track.name }}</p>
+        <p class="line-clamp-2">{{ getArtistNames(track.track.artists) }}</p>
+      </div>
     </div>
   </div>
 </template>
