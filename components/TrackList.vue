bd9a8b768afad5708f0ed138feb229f4720123a3
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-06-29 15:30:05 +0200

    a bit responsiveness

--- components/TrackList.vue
+++ components/TrackList.vue
@@ -8,22 +8,27 @@ const getArtistNames = (artists: any[]) => {
 </script>
 
 <template>
-  <div v-if="playlist" class="flex flex-wrap justify-between">
+  <div
+    v-if="playlist"
+    class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 px-8"
+  >
     <div
       v-for="(track, index) in playlist"
       :key="index"
-      class="w-1/8 bg-gray-200/10 m-2 flex flex-col"
+      class="aspect-1/1 bg-gray/20 w-100%"
     >
       <nuxt-img
         v-if="track.track.album.images && track.track.album.images[0]"
         :src="track.track.album.images[0].url"
         alt="cover"
         loading="lazy"
-        class="object-contain w-full"
+        class="object-contain w-100%"
       />
-      <div class="flex-grow">
-        <p class="line-clamp-2">{{ track.track.name }}</p>
-        <p class="line-clamp-2">{{ getArtistNames(track.track.artists) }}</p>
+      <div class="p-2 text-white/80">
+        <p class="line-clamp-1">
+          {{ track.track.name }}
+        </p>
+        <p class="line-clamp-1">{{ getArtistNames(track.track.artists) }}</p>
       </div>
     </div>
   </div>
