6c72de6d252706d9c194df6e2feedf6f2861ae0b
Marc Frankfurter <marcfrankfurter@gmail.com>
2023-07-24 21:32:41 +0200

    better placeholder for users without picture

--- components/UserPanel.vue
+++ components/UserPanel.vue
@@ -25,13 +25,18 @@ const formattedDuration = computed(() => msToTime(duration))
   <div v-if="userData" class="flex flex-col items-center gap-1 k-border p-4">
     <img
       v-if="userData.images && userData.images.length > 0"
-      class="rounded-full"
+      class="rounded-full border-gray/66 border-solid"
       :src="userData.images[0].url"
       alt="User profile image"
       width="60"
       height="60"
     />
-    <div v-else class="w-60px h-60px bg-gray rounded-full"></div>
+    <div
+      v-else
+      class="w-60px h-60px rounded-full flex items-center justify-center text-white border-gray/66 border-solid"
+    >
+      {{ userData.display_name.slice(0, 3) }}
+    </div>
     <div class="mt-2">{{ userData.display_name }}</div>
     <div class="opacity-80">{{ count }}</div>
     <div class="opacity-80">{{ formattedDuration }}</div>
